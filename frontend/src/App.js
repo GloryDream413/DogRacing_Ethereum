4f1fc8da85671e9063a9e3266d54ae6788d555d1
GloryDream413 <glorydream413@gmail.com>
2023-12-16 20:47:11 +0900

    Remove unnecessary files.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 // styles for this kit
