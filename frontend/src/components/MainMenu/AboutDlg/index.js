247d2ec4d273d0051eb7548fb0caf91390b4353e
root <root@api.peeke.ch>
2023-12-18 06:07:35 +0300

    Currency letter changed to ETH.

--- frontend/src/components/MainMenu/AboutDlg/index.js
+++ frontend/src/components/MainMenu/AboutDlg/index.js
@@ -87,7 +87,7 @@ function AboutDlg({
                 }}>Please choose an amount to deposit.</p>
                 <Box sx={{ width: 250, marginTop: '20px'}}>
                     <div className=''>
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount To Deposit(ℏ):</label>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount To Deposit(ETH):</label>
                         <input 
                             type="number"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
