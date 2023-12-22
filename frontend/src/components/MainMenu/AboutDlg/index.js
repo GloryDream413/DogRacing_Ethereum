8a9dcaa1f8adfc75d08c0ce2b2f58f766db7a98a
GloryDream413 <putnikdeveloperbg@gmail.com>
2023-12-22 12:48:37 +0300

    Currency ETH is removed.

--- frontend/src/components/MainMenu/AboutDlg/index.js
+++ frontend/src/components/MainMenu/AboutDlg/index.js
@@ -87,7 +87,7 @@ function AboutDlg({
                 }}>Please choose an amount to deposit.</p>
                 <Box sx={{ width: 270, marginTop: '10px'}}>
                     <div className=''>
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount To Deposit(ETH):</label>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount To Deposit:</label>
                         <input 
                             type="number"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
